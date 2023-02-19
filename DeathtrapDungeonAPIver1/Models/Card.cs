2d5e286a672b53f92ecdfc215413130e5532d44c
lexxA87 <alex.o.nikonov@gmail.com>
2023-02-19 20:55:26 +0400

    Added Card Model

---
+++ DeathtrapDungeonAPIver1/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeathtrapDungeonAPIver1.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+
+        [Key]
+        [Column(TypeName = "SMALLINT")]
+        public int Number { get; set; }
+
+        [Required]
+        [Column(TypeName = "NVARCHAR")]
+        public string Description { get; set; } = string.Empty;
+
+        public List<int>? GoToNumber { get; set; }
+    }
+}
