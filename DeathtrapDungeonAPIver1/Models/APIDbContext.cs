28df4fbe6ff4db4c48769dd6e0ae66fb7732b44a
lexxA87 <alex.o.nikonov@gmail.com>
2023-02-19 21:01:55 +0400

    Added db Context

---
+++ DeathtrapDungeonAPIver1/Models/APIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DeathtrapDungeonAPIver1.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions option) : base(option) { }
+
+        public DbSet<Card> Cards { get; set; }
+    }
+}
